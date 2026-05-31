481036b5fda4569af77ec9e224c3a2593f0eead7
OyeMensah <2425402995@live.gctu.edu.gh>
2026-05-31 17:12:08 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,36 @@
+# File: initials.py
+
+#Description: Print out my initials
+#Assignment Number: 1
+#
+#Name: Oye Nyamekye Mensah
+#STUDENT ID: 2425402995
+#Email:2425402995@live.gctu.edu.gh
+#Grader:Augustus
+#
+#On my honor, Oye Nyamekye Mensah, this programming assignment is my work
+#and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+    #Print my initials
 
+
+    print()
+    print("...ONM")
+    print()
+
+    print("...OOOOOOOOOOOO........NN........NN........MM........MM.....")
+    print("...OO........OO........NNN.......NN........MMM......MMM.....")
+    print("...OO........OO........NN.N......NN........MM.M....M.MM.....")
+    print("...OO........OO........NN..N.....NN........MM..M..M..MM.....")
+    print("...OO........OO........NN...N....NN........MM...MM...MM.....")
+    print("...OO........OO........NN....N...NN........MM........MM.....")
+    print("...OO........OO........NN.....N..NN........MM........MM.....")
+    print("...OO........OO........NN......N.NN........MM........MM.....")
+    print("...OO........OO...**...NN.......NNN...**...MM........MM...**")
+    print("...OOOOOOOOOOOO...**...NN........NN...**...MM........MM...**")
+
+    print()
 
 
 main()
